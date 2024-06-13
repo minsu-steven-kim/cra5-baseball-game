765fe47fedcd170c7627ed341c82e7dc53c68945
Minsu Kim <minsu.steven.kim@gmail.com>
2024-06-13 14:31:42 +0900

    [refactor] cycle 6

--- test_game.py
+++ test_game.py
@@ -15,6 +15,15 @@ class TestGame(TestCase):
         except TypeError:
             pass
 
+    def assert_correct_result(self, result, solved, strikes, balls):
+        self.assertIsNotNone(result)
+        self.assertEqual(solved, result.get_solved())
+        self.assertEqual(strikes, result.get_strikes())
+        self.assertEqual(balls, result.get_balls())
+
+    def generate_question(self, question):
+        self.game.question = question
+
     def test_exception_when_invalid_input(self):
         test_cases = [None, '12', '1234', '12s', '121']
         for tc in test_cases:
@@ -22,19 +31,9 @@ class TestGame(TestCase):
                 self.assert_legal_argument(tc)
 
     def test_result_when_matched_input(self):
-        self.game.question = '123'
-        result: GameResult = self.game.guess('123')
-
-        self.assertIsNotNone(result)
-        self.assertTrue(result.get_solved())
-        self.assertEqual(3, result.get_strikes())
-        self.assertEqual(0, result.get_balls())
+        self.generate_question('123')
+        self.assert_correct_result(self.game.guess('123'), True, 3, 0)
 
     def test_result_when_unmatched_input(self):
-        self.game.question = '123'
-        result: GameResult = self.game.guess('456')
-
-        self.assertIsNotNone(result)
-        self.assertFalse(result.get_solved())
-        self.assertEqual(0, result.get_strikes())
-        self.assertEqual(0, result.get_balls())
+        self.generate_question('123')
+        self.assert_correct_result(self.game.guess('456'), False, 0, 0)
