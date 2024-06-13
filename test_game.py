cc2687c26dcede29dfd9b8d9e6d6bae7a310a5bc
Minsu Kim <minsu.steven.kim@gmail.com>
2024-06-13 13:49:11 +0900

    [feature] cycle 1

--- test_game.py
+++ test_game.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
 
+from game import Game
+
 
 class TestGame(TestCase):
-    def test_game(self):
-        self.assertEqual(1, 1)
+    def setUp(self):
+        super().setUp()
+        self.game = Game()
+
+    def test_exception_when_input_is_none(self):
+        with self.assertRaises(TypeError):
+            self.game.guess(None)
